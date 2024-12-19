ed92c4591b66ebf77d35cf7c6747e22b04627378
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-12-19 08:26:24 -0800

    task (api): searchByISBN

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OpenlibraryService } from './openlibrary.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'library';
+
+  constructor(private openlibrary: OpenlibraryService) {}
+
+  ngOnInit(): void {
+    this.openlibrary.searchByISBN().subscribe((res: any) => console.log(res));
+  }
 }
--- src/app/openlibrary.service.ts
+++ src/app/openlibrary.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface ISBNSearchResult {}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OpenlibraryService {
+  constructor(private http: HttpClient) {}
+  private baseUrl = 'https://openlibrary.org';
 
-  constructor() { }
+  public searchByISBN(
+    isbn: string = '9781680507225'
+  ): Observable<ISBNSearchResult> {
+    return this.http.get(`${this.baseUrl}/isbn/${isbn}.json`);
+  }
 }
