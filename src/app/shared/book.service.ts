db356e39ba57cdbd951b9a86301fa8e079fe309f
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-12-19 13:32:42 -0800

    task: book service contains CRUD operations for book management

--- src/app/shared/book.service.ts
+++ src/app/shared/book.service.ts
@@ -7,8 +7,46 @@ import { Book } from './types';
 export class BookService {
   private localStorageKey = 'bookLibrary';
 
+  getAllBooks(): Book[] {
+    const storedBooks = localStorage.getItem(this.localStorageKey);
+    return storedBooks ? JSON.parse(storedBooks) : [];
+  }
+
   addBook(book: Book) {
-    localStorage.setItem(this.localStorageKey, JSON.stringify([book]));
-    console.log(book)
+    const books = this.getAllBooks();
+    // Don't add if book exists
+    if (this.isBookInLibrary(book.isbn)) {
+      console.warn(`Book with ISBN ${book.isbn} already exists in library.`);
+      return;
+    }
+    localStorage.setItem(
+      this.localStorageKey,
+      JSON.stringify([...books, book])
+    );
+    console.log(book);
+  }
+
+  getBook(isbn: string): Book | undefined {
+    return this.getAllBooks().find((book) => book.isbn === isbn);
+  }
+
+  updateBook(book: Book): void {
+    const books = this.getAllBooks();
+    const updatedBooks = books.map((b) => {
+      if (b.isbn === book.isbn) {
+        return book;
+      }
+      return b;
+    });
+    localStorage.setItem(this.localStorageKey, JSON.stringify(updatedBooks));
+  }
+
+  deleteBook(isbn: string): void {
+    const books = this.getAllBooks().filter((book) => book.isbn !== isbn);
+    localStorage.setItem(this.localStorageKey, JSON.stringify(books));
+  }
+
+  isBookInLibrary(isbn: string): boolean {
+    return !!this.getBook(isbn);
   }
 }
