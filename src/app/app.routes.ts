2075a53e848243f3a3a099f4a475a09aafd08b41
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-12-19 10:05:30 -0800

    task: setup routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './pages/list/list.component';
+import { BookSearchComponent } from './pages/book-search/book-search.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ListComponent },
+  { path: 'search', component: BookSearchComponent },
+];
