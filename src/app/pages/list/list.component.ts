d3a285a1019b1460e5e8b2c40eb85dcaa63f3fa2
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-12-19 14:00:35 -0800

    feat: list books

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../../shared/types';
+import { BookService } from '../../shared/book.service';
 
 @Component({
   selector: 'app-list',
   imports: [],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrl: './list.component.css',
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+  bookList: Book[] = [];
 
+  constructor(private books: BookService) {}
+
+  ngOnInit(): void {
+    this.bookList = this.books.getAllBooks();
+  }
 }
