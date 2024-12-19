c6b4e4d334edfa900bbe753c8627f2b0df7570dd
migue <35717991+mrivasperez@users.noreply.github.com>
2024-12-19 09:56:17 -0800

    feat: ISBN Search (#1)
    * chore: add bootstrap
    
    * chore: add bootstrap icons
    
    * task: validate stylesheet
    
    * task: scaffold openlibrary service
    
    * task (api): searchByISBN
    
    * task: book lookup proof of concept
    
    * task: create function to generate book cover url
    
    * refactor: use baseUrl for api call
    
    * ui: isbn input elements
    
    * ui: error state
    
    * refactor: service has no default arg
    
    * task: error handling for handleFetchBookData
    
    * task: remove unnecessary lib function
    
    * task (api): search author by olid
    
    * ui: author and book cover
    
    * task: scaffold book-search component
    
    * refactor: remove book-search ui and logic from app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { BookSearchComponent } from './pages/book-search/book-search.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [BookSearchComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'library';
---
+++ src/app/openlibrary.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface ISBNSearchResult {
+  publishers: string[];
+  subtitle: string;
+  title: string;
+  number_of_pages: number;
+  publish_date: string;
+  authors: { key: string }[];
+  covers: string[];
+}
+
+export interface AuthorSearchResult {
+  name: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OpenlibraryService {
+  constructor(private http: HttpClient) {}
+  private baseUrl = 'https://openlibrary.org';
+
+  public searchByISBN(isbn: string) {
+    return this.http.get(
+      `${this.baseUrl}/isbn/${isbn}.json`
+    ) as Observable<ISBNSearchResult>;
+  }
+
+  public searchAuthor(key: string) {
+    return this.http.get(
+      `${this.baseUrl}${key}.json`
+    ) as Observable<AuthorSearchResult>;
+  }
+}
