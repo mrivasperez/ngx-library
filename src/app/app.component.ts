9438d02d0012cc97edb78c8c977757ab03c47d8a
migue <35717991+mrivasperez@users.noreply.github.com>
2024-12-20 09:02:21 -0800

    feat: Theme (#11)
    * task: create theme service
    
    * task: use them service in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ThemeService } from './theme.service';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,6 @@ import { RouterOutlet } from '@angular/router';
   template: '<router-outlet />',
 })
 export class AppComponent {
+  constructor(public themeService: ThemeService) {}
   title = 'library';
 }
