6a9814e4483665b111fea89716efbedb74b281f5
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-12-20 09:34:03 -0800

    task: prototype scan (success!)

--- src/app/pages/scan/scan.component.ts
+++ src/app/pages/scan/scan.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { BarcodeFormat } from '@zxing/library';
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
 
 @Component({
   selector: 'app-scan',
-  imports: [],
+  imports: [ZXingScannerModule],
   templateUrl: './scan.component.html',
-  styleUrl: './scan.component.css'
+  styleUrl: './scan.component.css',
 })
 export class ScanComponent {
+  allowedFormat = [BarcodeFormat.EAN_13];
 
+  onScanSuccess(scan: string){
+    console.log(scan)
+  }
 }
