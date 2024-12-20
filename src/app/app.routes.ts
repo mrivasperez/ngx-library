201ee855facdec6358027609de74fed93db72f29
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-12-20 09:17:22 -0800

    task: add scan route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { ListComponent } from './pages/list/list.component';
 import { BookSearchComponent } from './pages/book-search/book-search.component';
 import { BookDetailsComponent } from './pages/book-details/book-details.component';
+import { ScanComponent } from './pages/scan/scan.component';
 
 export const routes: Routes = [
   { path: '', component: ListComponent },
   { path: 'search', component: BookSearchComponent },
   { path: 'books/:isbn', component: BookDetailsComponent },
+  { path: 'scan', component: ScanComponent },
 ];
