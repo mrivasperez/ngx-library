76a8cfa752e19e5882baeae98fb2f80c58fc42e1
Ahmed Aziz <tonyg7703@gmail.com>
2025-01-14 15:11:30 -0800

    task: load filtered Navbar routes on init Co-authored-by: migue <mrivasperez@users.noreply.github.com>

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { routes, AppRoute } from '../../../app.routes';
 
 @Component({
   selector: 'app-navbar',
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  routes: AppRoute[] = [];
 
+  ngOnInit() {
+    this.routes = routes.filter((route) => route.data?.showInNavbar);
+  }
 }
